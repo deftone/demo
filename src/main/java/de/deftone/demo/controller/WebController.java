31b08cd089b314fd94818727f9628eec4f2908da
deftone <katrin.rose@poste.de>
2022-03-06 16:04:56 +0100

    alte endpoints vorruebergehend abgeschaltet und presse noch hinzugefuegt

--- src/main/java/de/deftone/demo/controller/WebController.java
+++ src/main/java/de/deftone/demo/controller/WebController.java
@@ -25,63 +25,67 @@ public class WebController {
 
     @GetMapping("/")
     public String showTemplate(Model model) {
-        model.addAttribute("nextEvent", eventService.getNextEvent().getFormattedDate());
-        model.addAttribute("locations", locationService.getAllLocations());
-        model.addAttribute("freeLocations", locationService.getFreeLocations());
-        model.addAttribute("participants", participantService.getAllParticipantsForNextEvent());
-        model.addAttribute("freeLocation", new FreeLocation());
-        model.addAttribute("givenLocation", new GivenLocation());
-        return "index";
+//        model.addAttribute("nextEvent", eventService.getNextEvent().getFormattedDate());
+//        model.addAttribute("locations", locationService.getAllLocations());
+//        model.addAttribute("freeLocations", locationService.getFreeLocations());
+//        model.addAttribute("participants", participantService.getAllParticipantsForNextEvent());
+//        model.addAttribute("freeLocation", new FreeLocation());
+//        model.addAttribute("givenLocation", new GivenLocation());
+//        return "index";
+        return "redirect:/aktionSaubereLandschaft";
     }
-
-    @PostMapping("/addPerson")
-    public String addPerson(@Valid @ModelAttribute GivenLocation givenLocation,
-                            BindingResult bindingResult,
-                            Model model) {
-
-        // das hier klappt, aber der fehlerrahmen erscheint nicht :(
-        if (bindingResult.hasErrors()) {
-            //todo: ein pop up? dass beides angegeben werden muss?
-            // oder etwas ins html hinzufuegen?
-            return "redirect:/#anmelden";
-        }
-
-        Participant participant = new Participant();
-        participant.setName(givenLocation.getName());
-        participant.setAngemeldetAm(LocalDate.now());
-        participant.setEvent(eventService.getNextEvent());
-        participant.setLocationName(locationService.getLocationNameById(givenLocation.getIdFromString()));
-        participantService.addParticipant(participant);
-
-        //und anschliessend als gebucht setzen, damit aus auswahlbox verschwindet
-        locationService.setLocationToBooked(givenLocation.getIdFromString());
-
-        return "redirect:/#mitmacher";
-    }
-
-    @PostMapping("/addPersonNewRoute")
-    public String addPersonNewRoute(@Valid @ModelAttribute FreeLocation freeLocation,
-                                    BindingResult bindingResult,
-                                    Model model) {
-        // das hier klappt, aber der fehlerrahmen erscheint nicht :(
-        if (bindingResult.hasErrors()) {
-            //todo: ein pop up? dass beides angegeben werden muss?
-            // oder etwas ins html hinzufuegen?
-            return "redirect:/#anmelden";
-        }
-
-        if (freeLocation != null
-                && !freeLocation.getName().isEmpty()
-                && !freeLocation.getLocation().isEmpty()) {
-            Participant participant = new Participant();
-            participant.setName(freeLocation.getName());
-            participant.setAngemeldetAm(LocalDate.now());
-            participant.setEvent(eventService.getNextEvent());
-            participant.setLocationName(freeLocation.getLocation());
-            participantService.addParticipant(participant);
-        }
-        return "redirect:/#mitmacher";
-    }
+// die urspruenglichen endpoints vorruebergehend ausschalten:
+//
+//    @PostMapping("/addPerson")
+//    public String addPerson(@Valid @ModelAttribute GivenLocation givenLocation,
+//                            BindingResult bindingResult,
+//                            Model model) {
+//
+//        // das hier klappt, aber der fehlerrahmen erscheint nicht :(
+//        if (bindingResult.hasErrors()) {
+//            //todo: ein pop up? dass beides angegeben werden muss?
+//            // oder etwas ins html hinzufuegen?
+//            return "redirect:/#anmelden";
+//            // kein redirect!            return "index";
+//        }
+//
+//        Participant participant = new Participant();
+//        participant.setName(givenLocation.getName());
+//        participant.setAngemeldetAm(LocalDate.now());
+//        participant.setEvent(eventService.getNextEvent());
+//        participant.setLocationName(locationService.getLocationNameById(givenLocation.getIdFromString()));
+//        participantService.addParticipant(participant);
+//
+//        //und anschliessend als gebucht setzen, damit aus auswahlbox verschwindet
+//        locationService.setLocationToBooked(givenLocation.getIdFromString());
+//
+//        return "redirect:/#mitmacher";
+//    }
+//
+//    @PostMapping("/addPersonNewRoute")
+//    public String addPersonNewRoute(@Valid @ModelAttribute FreeLocation freeLocation,
+//                                    BindingResult bindingResult,
+//                                    Model model) {
+//        // das hier klappt, aber der fehlerrahmen erscheint nicht :(
+//        if (bindingResult.hasErrors()) {
+//            //todo: ein pop up? dass beides angegeben werden muss?
+//            // oder etwas ins html hinzufuegen?
+//            return "redirect:/#anmelden";
+//// kein redirect!            return "index";
+//        }
+//
+//        if (freeLocation != null
+//                && !freeLocation.getName().isEmpty()
+//                && !freeLocation.getLocation().isEmpty()) {
+//            Participant participant = new Participant();
+//            participant.setName(freeLocation.getName());
+//            participant.setAngemeldetAm(LocalDate.now());
+//            participant.setEvent(eventService.getNextEvent());
+//            participant.setLocationName(freeLocation.getLocation());
+//            participantService.addParticipant(participant);
+//        }
+//        return "redirect:/#mitmacher";
+//    }
 
     @GetMapping({"/presse"})
     public String presse() {
