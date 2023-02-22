7d8ccb736c90abdb65df7db7d3afc2f5e741b372
deftone <katrin.rose@poste.de>
2023-02-22 18:19:03 +0100

    pop up

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -16,3 +16,24 @@ $(document).ready(function() {
 	});
 
 });
+
+$(function(){
+var overlay = $('<div id="overlay"></div>');
+overlay.show();
+overlay.appendTo(document.body);
+$('.popup').show();
+$('.close').click(function(){
+$('.popup').hide();
+overlay.appendTo(document.body).remove();
+return false;
+});
+
+
+
+
+$('.x').click(function(){
+$('.popup').hide();
+overlay.appendTo(document.body).remove();
+return false;
+});
+});
